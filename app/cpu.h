c457a86796be2b3315d37194acc6fc1801d36d12
olesgedz <gedzoles@gmail.com>
2022-01-01 14:00:57 +0300

    DR

--- app/cpu.h
+++ app/cpu.h
@@ -1,183 +1,126 @@
-#pragma once
+#ifndef INCLUDED_CPU_H
+#define INCLUDED_CPU_H
 
-#include "common.h"
+#include <cstdint>
+struct Dromaius;
 
-namespace dsemu {
-namespace cpu {
-
-struct Register {
-    byte lo;
-    byte hi;
-};
+#define CPU_CALL_STACK_SIZE 0x100
 
 /*
 
-The Zero Flag (Z)
-This bit becomes set (1) if the result of an operation has been zero (0). Used for conditional jumps.
-
-The Carry Flag (C, or Cy)
-Becomes set when the result of an addition became bigger than FFh (8bit) or FFFFh (16bit). Or when the result of a subtraction or comparision became less than zero (much as for Z80 and 80x86 CPUs, but unlike as for 65XX and ARM CPUs). Also the flag becomes set when a rotate/shift operation has shifted-out a "1"-bit.
-Used for conditional jumps, and for instructions such like ADC, SBC, RL, RLA, etc.
-
-The BCD Flags (N, H)
-These flags are (rarely) used for the DAA instruction only, N Indicates whether the previous instruction has been an addition or subtraction, and H indicates carry for lower 4bits of the result, also for DAA, the C flag must indicate carry for upper 8bits.
-After adding/subtracting two BCD numbers, DAA is intended to convert the result into BCD format; BCD numbers are ranged from 00h to 99h rather than 00h to FFh.
-Because C and H flags must contain carry-outs for each digit, DAA cannot be used for 16bit operations (which have 4 digits), or for INC/DEC operations (which do not affect C-flag).
-
+GB CPU freq:                 4194304 Hz
+m-cycles per sec:            1048576 Hz
+DIV register, increments at:   16384 Hz (every 64 m-cycles)
 */
 
-enum Flags {
-    FlagZ = 7,
-    FlagN = 6,
-    FlagH = 5,
-    FlagC = 4
+
+struct CPU
+{
+	enum Int {
+		VBLANK  = 0x01,
+		LCDSTAT = 0x02,
+		TIMER   = 0x04,
+		SERIAL  = 0x08,
+		JOYPAD  = 0x10
+	};
+
+	enum Flag {
+		ZERO      = 0x80,
+		SUBTRACT  = 0x40,
+		HCARRY    = 0x20,
+		CARRY     = 0x10,
+	};
+
+	struct regs_s {
+		uint8_t a;
+		uint8_t b;
+		uint8_t c;
+		uint8_t d;
+		uint8_t e;
+		uint8_t h;
+		uint8_t l;
+		uint8_t f;		// flags
+		uint16_t pc;	// program counter
+		uint16_t sp;	// stack pointer
+	};
+
+	struct timer_s {
+		uint8_t div;
+		uint8_t tima;
+		uint8_t tma;
+		uint8_t tac;
+		unsigned long long cycleCount;
+		int cycleCountDiv;
+		int maxCount[4]; // const
+	};
+
+	// Up-reference
+	Dromaius *emu;
+
+	regs_s r;
+	regs_s registerStore;
+
+	bool intsOn;
+	uint8_t intFlags;
+	uint8_t oldIntFlags;
+	uint8_t ints;
+	timer_s timer;
+
+	bool halted;
+
+	bool fastForward;
+	bool stepMode;
+	bool stepInst;
+	bool stepFrame;
+
+	// Debugging
+	uint16_t callStack[CPU_CALL_STACK_SIZE];
+	int callStackDepth;
+
+	// Cycle count
+	unsigned long long c;
+	unsigned long long dc;
+
+	void initialize();
+
+	void setFlag(Flag flag);
+	void resetFlag(Flag flag);
+	int getFlag(Flag flag);
+	void doIncReg(uint8_t *regp);
+	void doDecReg(uint8_t *regp);
+	void doAddReg(uint8_t *regp, uint8_t val);
+	void doAddRegWithCarry(uint8_t *regp, uint8_t val);
+	void doSubReg(uint8_t *regp, uint8_t val);
+	void doSubRegWithCarry(uint8_t *regp, uint8_t val);
+	void doAddHL(uint16_t val);
+	void doRotateLeftWithCarry(uint8_t *regp);
+	void doRotateRightWithCarry(uint8_t *regp);
+	void doRotateLeft(uint8_t *regp);
+	void doRotateRight(uint8_t *regp);
+	void doShiftLeft(uint8_t *regp);
+	void doShiftRight(uint8_t *regp);
+	void doShiftRightL(uint8_t *regp);
+	void doSwapNibbles(uint8_t *regp);
+	void doBit(uint8_t *regp, uint8_t bit);
+	void doRes(uint8_t *regp, uint8_t bit);
+	void doSet(uint8_t *regp, uint8_t bit);
+	void doAndRegA(uint8_t val);
+	void doXorRegA(uint8_t val);
+	void doOrRegA(uint8_t val);
+	void doCpRegA(uint8_t val);
+	void doOpcodeUNIMP();
+	uint8_t *numToRegPtr(uint8_t num);
+	void printRegisters();
+	void doExtraOP(uint8_t op);
+	void handleTimers();
+	void handleInterrupts();
+	int executeInstruction();
+	inline const char *numToRegName(uint8_t num);
+	uint16_t instructionToString(uint16_t pc, char *instStr);
+	void disassemble(uint16_t pc, size_t instCnt, char *buf);
+
+	void callStackPush(uint16_t oldpc, uint16_t pc);
+	void callStackPop(uint16_t oldpc, uint16_t pc);
 };
 
-enum Op {
-    NOP,
-    LD,
-    LDI,
-    LDD,
-    INC,
-    DEC,
-    JR,
-    DAA,
-    ADD,
-    SUB,
-    RLA,
-    RLCA,
-    RRA,
-    RRCA,
-    STOP,
-    SCF,
-    CPL,
-    CCF,
-    SBC,
-    XOR,
-    AOR,
-    CP,
-    POP,
-    PUSH,
-    AND,
-    OR,
-    JP,
-    RET,
-    DI,
-    EI,
-    CALL,
-    RST,
-    CB,
-    ADC,
-    X,
-    RETI,
-    LDH,
-    HALT
-};
-
-enum AddrType {
-    ATypeNA,
-    ATypeRR,
-    ATypeIR,
-    ATypeAR,
-    ATypeRI,
-    ATypeRA,
-    ATypeJ,
-    ATypeJ_NZ,
-    ATypeJ_Z,
-    ATypeJ_NC,
-    ATypeJ_C,
-    ATypeR,
-    ATypeA,
-    ATypeSP
-};
-
-enum ParamType {
-    A,
-    B,
-    C,
-    D,
-    E,
-    H,
-    L,
-    BC,
-    DE,
-    HL,
-    SP,
-    PC,
-    N,
-    NN,
-    AF,
-    x00, x10, x20, x30, x08, x18, x28, x38
-};
-
-enum Interrupts {
-    IVBlank = 1
-};
-
-struct OpCode {
-    byte value;
-    const string &name;
-    Op op;
-    byte length;
-    byte cycles;
-    AddrType mode;
-    ParamType params[4];
-};
-
-extern Register regAF;
-extern Register regBC;
-extern Register regDE;
-extern Register regHL;
-extern ushort regPC;
-extern Register regSP;
-
-extern OpCode opCodes[];
-
-extern int extraCycles;
-extern bool haltWaitingForInterrupt;
-
-inline bool getFlag(Flags n) {
-    return getBit(regAF.lo, n);
-}
-
-inline void setFlag(Flags n, bool val) {
-    setBit(regAF.lo, n, val);
-}
-
-inline ushort *getReg16Pointer(const Register &reg) {
-    return (ushort *)&reg;
-}
-
-inline ushort getReg16Value(const Register &reg) {
-    return *getReg16Pointer(reg);
-}
-
-inline void setReg16Value(const Register &reg, ushort val) {
-    *getReg16Pointer(reg) = val;
-}
-
-extern bool paused;
-
-void run();
-void handleInterrupt(byte flag, bool request, bool pcp1 = true);
-void tick();
-void init();
-void interrupt(Interrupts i);
-
-byte getInterruptsEnableFlag();
-byte getInterruptsRequestsFlag();
-
-void setInterruptsEnableFlag(byte f);
-void setInterruptsRequestsFlag(byte f);
-
-uint64_t getTickCount();
-void changePC(ushort address);
-
-byte pop();
-ushort spop();
-void push(byte);
-void push(ushort);
-
-}
-}
-
+#endif
--- app/emu.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-namespace dsemu {
-
-void run();
-
-}
